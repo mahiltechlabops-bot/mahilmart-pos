9ed3305c5ca7330692a3f92bf87c2833a6dd5258
Ranjithkumar1010 <ranjithkumar.k.mahiltechlab@gmail.com>
2025-07-04 13:47:13 +0530

    model _updates

--- MahilMartPOS_App/models.py
+++ MahilMartPOS_App/models.py
@@ -1,11 +1,4 @@
 from django.db import models
 
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.CharField(max_length=100, default = 'kg')
-
-
-    def __str__(self):
-        return self.name
-
+def __str__(self):
+    return self.name
