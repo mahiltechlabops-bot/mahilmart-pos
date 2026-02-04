1805daa14d1e37ab3886ad4a8b2ad5ef7c852215
praveen <praveen.v@mahiltechlab.com>
2026-02-04 17:36:38 +0530

    commit all

--- build_exe.ps1
+++ build_exe.ps1
@@ -2,12 +2,4 @@ $ErrorActionPreference = "Stop"
 
 python -m pip install --upgrade pip pyinstaller
 
-python -m PyInstaller --clean --noconfirm --onefile --noconsole --name MahilMartPOS `
-  --collect-all django `
-  --collect-all barcode `
-  --add-data "db.sqlite3;." `
-  --add-data "MahilMartPOS_App\\templates;MahilMartPOS_App\\templates" `
-  --add-data "staticfiles;staticfiles" `
-  --add-data "MahilMartPOS;MahilMartPOS" `
-  --add-data "MahilMartPOS_App;MahilMartPOS_App" `
-  app_launcher.py
+python -m PyInstaller --clean --noconfirm MahilMartPOS.spec
