0565a35ce986e6bb046752e26757d28624c6be38
praveen <praveen.v@mahiltechlab.com>
2026-02-04 17:11:43 +0530

    commit all
    commit all

--- build_exe.ps1
+++ build_exe.ps1
@@ -5,7 +5,7 @@ python -m pip install --upgrade pip pyinstaller
 python -m PyInstaller --clean --noconfirm --onefile --noconsole --name MahilMartPOS `
   --collect-all django `
   --collect-all barcode `
-  --add-data "db.sqlite3;db.sqlite3" `
+  --add-data "db.sqlite3;." `
   --add-data "MahilMartPOS_App\\templates;MahilMartPOS_App\\templates" `
   --add-data "staticfiles;staticfiles" `
   --add-data "MahilMartPOS;MahilMartPOS" `
